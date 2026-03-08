446f6685a68f5b941ff70d8dc6d64c2fdf119349
MIGUEL <anthonymiguelochoa03@gmail.com>
2026-03-08 05:08:13 -0700

    Fixes

--- Assets/LoseBarrier/LoseBarrierScript.cs
+++ Assets/LoseBarrier/LoseBarrierScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class LoseBarrierScript : NetworkBehaviour
 {
-    //if (!IsServer) return;
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
