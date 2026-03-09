f4a645674ce7d91c9225cef1e7bd570c5684fecb
MIGUEL <anthonymiguelochoa03@gmail.com>
2026-03-08 17:41:30 -0700

    Fixed Issues With Computers Connecting

--- Assets/Host_Client/NetworkUI.cs
+++ Assets/Host_Client/NetworkUI.cs
@@ -5,11 +5,17 @@ public class NetworkUI : MonoBehaviour
 {
     public void StartHost()
     {
+        var transport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+        //transport.ConnectionData.Address = "192.168.1.226";
+        transport.ConnectionData.Address = "192.168.1.235";
         NetworkManager.Singleton.StartHost();
     }
 
     public void StartClient()
     {
+        var transport = NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>();
+        //transport.ConnectionData.Address = "192.168.1.226";
+        transport.ConnectionData.Address = "192.168.1.235";
         NetworkManager.Singleton.StartClient();
     }
 }
--- Assets/LoseBarrier/LoseBarrierScript.cs
+++ Assets/LoseBarrier/LoseBarrierScript.cs
@@ -5,6 +5,7 @@ public class LoseBarrierScript : NetworkBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
+        if (!IsServer) return;
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(collision.gameObject);
